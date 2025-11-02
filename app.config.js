bc36efdb57a64b93fae4d08ae739282bee600839
clangauge0314 <clangauge0314@naver.com>
2025-11-02 12:55:29 +0900

    2025-11-02 12:55

---
+++ app.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  expo: {
+    name: "my-nativewind-app",
+    slug: "my-nativewind-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "mynativewindapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.mynativewindapp",
+      versionCode: 1,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON, 
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+      edgeToEdgeEnabled: false,
+      predictiveBackGestureEnabled: false,
+      softwareKeyboardLayoutMode: "pan",
+      navigationBar: {
+        visible: "leanback"
+      },
+      statusBar: {
+        hidden: false,
+        style: "auto"
+      }
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000"
+          }
+        }
+      ],
+      "expo-font"
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "f7a248cc-ffbe-4967-8a18-2476fc75f286"
+      }
+    }
+  }
+};
